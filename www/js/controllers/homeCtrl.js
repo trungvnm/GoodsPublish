a3fc0fe50ad42d7759a21f9edc264d3880370e7c
thangtv <thangtv@DESKTOP-JSF17OV>
2017-02-21 09:42:38 +0700

    update code: add module

---
+++ www/js/controllers/homeCtrl.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    angular.module("LelongApp.controllers").controller('homeCtrl', ['$scope', homeCtrl]);
+    /**
+        home controller constructor
+	 */
+    function homeCtrl($scope) {
+        //init data
+        init();
+    }
+    function init() {
+        console.log('start app');
+    }
+
+})();
